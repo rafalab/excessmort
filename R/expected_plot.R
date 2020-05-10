1cf6756550a0e19fd99f1e390b91dca1b09486cf
Rafael A. Irizarry <rairizarry@gmail.com>
2020-05-10 01:07:18 -0400

    adding a plot for checking expected counts

---
+++ R/expected_plot.R
@@ -0,0 +1,48 @@
+#' Check if expected counts fit data
+#' @export
+#' @importFrom stats qnorm
+#' @importFrom ggplot2 ggplot geom_ribbon ylab xlab geom_point coord_cartesian geom_line
+#'
+
+
+expected_plot <- function(expected, title = "",
+                          start = NULL,
+                          end = NULL,
+                          ylim = NULL,
+                          weekly = TRUE,
+                          color = "#3366FF",
+                          alpha = 1){
+
+  requireNamespace("ggplot2")
+
+  if(is.null(start)) start <- min(expected$date)
+  if(is.null(end)) end <- max(expected$date)
+
+  dat <- with(expected,
+              data.frame(date = date,
+                         observed = observed,
+                         expected = expected)) %>%
+    filter(date >= start & date <= end)
+  if(weekly){
+    dat <- dat %>%
+      mutate(date = floor_date(date, unit = "week")) %>%
+      group_by(date) %>%
+      summarize(expected = sum(expected),
+                observed = sum(observed)) %>%
+      ungroup()
+  }
+
+  yl<- "Counts"
+  if(weekly) yl <- paste("Weekly", yl)
+  p <- dat %>%
+    ggplot(aes(x = date)) +
+    geom_point(aes(y = observed), alpha = alpha) +
+    geom_line(aes(y = expected), color = color) +
+    ylab(yl) +
+    ggtitle(title) +
+    xlab("Date")
+
+  if(!is.null(ylim)) p <- p + coord_cartesian(ylim = ylim)
+
+  return(p)
+}

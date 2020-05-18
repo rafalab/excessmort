e2b7f634db63a76a4dc68e934e25abeb7ff6d5f0
RJNunez <roa310@g.harvard.edu>
2020-05-18 17:02:53 -0400

    updates to approx_demographics

--- R/approx_demographics.R
+++ R/approx_demographics.R
@@ -2,14 +2,14 @@
 #' @export
 #' @importFrom stats approx
 #' @import dplyr
-approx_demographics <- function(demo, first_day, last_day)
+approx_demographics <- function(demo, first_day, last_day, by=c("year"))
 {
   ################## ----- PARAMETERS ----- ##################
   # 1. demo      : Dataframe from the function getDemographics()
   # 2. first_day : First day of interpolating sequence
   # 3. last_day  : Last day of interpolating sequence
 
-   # -- Defining xout days
+  # -- Defining xout days
   first_day <- as.Date(first_day)
   last_day  <- as.Date(last_day)
   days <- seq(first_day, last_day, by=1)
@@ -27,12 +27,13 @@ approx_demographics <- function(demo, first_day, last_day)
 
   # -- Interpolating population
   demo <- demo %>%
-    group_by(sex, agegroup, race) %>%
+    group_by_at(by) %>%
+    summarize(population = sum(population, na.rm=T)) %>%
+    ungroup() %>%
+    group_by_at(by[-1]) %>%
     do(do_approx(., days)) %>%
     ungroup() %>%
-    select(date, sex, race, agegroup, population)
+    as_tibble()
 
   return(demo)
 }
-
-

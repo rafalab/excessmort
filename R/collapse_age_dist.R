2b8e2b41ca24045dd48df8a137ee5274515d991f
Rafael A. Irizarry <rairizarry@gmail.com>
2020-05-18 23:33:31 -0400

    addin collapse_counts_by_age

--- R/collapse_age_dist.R
+++ R/collapse_age_dist.R
@@ -31,3 +31,35 @@ collapse_age_dist <- function(demo, breaks){
            arrange(agegroup) %>%
            drop_na(agegroup))
 }
+
+
+
+#' @rdname collapse_age_dist
+
+collapse_counts_by_age <- function(counts, breaks){
+  ## assumes groups are start-end
+  ## assumes agegroup contains group
+  ## assumes column named population has population values
+  ## assumes column names outcome has outcome values
+
+  counts <- separate(counts, agegroup, c("start", "end"), sep = "-", remove = FALSE, convert = TRUE)
+
+  edges <- c(unique(counts$start), Inf)
+
+  if(!all(breaks %in% edges)) stop("Age breaks must be a subset of", paste(edges, collapse = ","))
+
+  g <- as.character(cut(counts$start, breaks, right = FALSE,
+                        labels = paste(breaks[-length(breaks)], c(breaks[-1]-1), sep="-")))
+
+  vars <- c(setdiff(names(counts), c("start", "end", "outcome", "population")))
+
+  return(counts %>% mutate(agegroup = g) %>%
+           group_by_at(vars) %>%
+           summarize(population = sum(population),
+                     outcome = sum(outcome)) %>%
+           ungroup() %>%
+           separate(agegroup, c("start", "end"), sep = "-", remove = FALSE, convert = TRUE) %>%
+           mutate(agegroup = reorder(agegroup, start, first)) %>%
+           select(-start, -end) %>%
+           drop_na(agegroup))
+}

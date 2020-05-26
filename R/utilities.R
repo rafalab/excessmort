ee1d37416c0e4855c3558eff71cc66a6f5e2bd68
Rafael A. Irizarry <rairizarry@gmail.com>
2020-05-25 20:53:21 -0400

    adding pseudo-global variables to avoid notes

---
+++ R/utilities.R
@@ -0,0 +1,25 @@
+#' @importFrom utils globalVariables
+globalVariables(
+  c(".",
+    "agegroup",
+    "population",
+    "start",
+    "end",
+    "outcome",
+    "predict",
+    "y",
+    "increase",
+    "se",
+    "observed",
+    "AGEGROUP",
+    "SEX",
+    "HISP",
+    "RACE",
+    "year",
+    "value",
+    "sex",
+    "race")
+)
+    
+    
+    

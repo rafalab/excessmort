2797e99097d55da98d84af1e91f61cbcbf6f9322
Rafael A. Irizarry <rairizarry@gmail.com>
2020-05-26 00:03:49 -0400

    adding doc dir

---
+++ doc/excessmort.R
@@ -0,0 +1,98 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+options(digits = 3)
+
+## -----------------------------------------------------------------------------
+library(dplyr)
+library(ggplot2)
+dslabs::ds_theme_set()
+library(excessmort)
+data("cook_records")
+head(cook_records)
+
+## -----------------------------------------------------------------------------
+head(cook_demographics)
+
+## -----------------------------------------------------------------------------
+counts <- compute_counts(cook_records)
+head(counts)
+
+## -----------------------------------------------------------------------------
+counts <- compute_counts(cook_records, demo = cook_demographics)
+head(counts)
+
+## -----------------------------------------------------------------------------
+counts <- compute_counts(cook_records, by = "agegroup", demo = cook_demographics, 
+                         breaks = c(0,20,40,60,80,Inf))
+head(counts)
+
+## -----------------------------------------------------------------------------
+counts <- compute_counts(cook_records, by = c("agegroup", "race", "sex"), 
+                         demo = cook_demographics, 
+                         breaks = c(0,20,40,60,80,Inf))
+head(counts)
+
+## ---- message=FALSE, warning=FALSE--------------------------------------------
+library(dplyr)
+library(lubridate)
+exclude_dates <- c(seq(make_date(2017, 12, 16), make_date(2018, 1, 16), by = "day"),
+                   seq(make_date(2020, 1, 1), max(cdc_state_counts$date), by = "day"))
+
+## -----------------------------------------------------------------------------
+counts <- cdc_state_counts %>% filter(state == "Massachusetts") %>%
+  compute_expected(exclude = exclude_dates, weekday.effect = FALSE)
+head(counts)
+
+## -----------------------------------------------------------------------------
+expected_plot(counts)
+
+## -----------------------------------------------------------------------------
+attr(counts, "dispersion")
+
+## -----------------------------------------------------------------------------
+res  <- cdc_state_counts %>% filter(state == "Massachusetts") %>%
+  compute_expected(exclude = exclude_dates, weekday.effect = FALSE,
+                   keep.components = TRUE)
+
+## -----------------------------------------------------------------------------
+qplot(res$counts$date, res$trend, geom = "line", 
+      xlab = "Date", ylab = "Death rate")
+
+## -----------------------------------------------------------------------------
+qplot(day, s, data = res$seasonal, geom = "line", 
+      xlab = "Day of the year", ylab = "Seconal effect")
+
+## -----------------------------------------------------------------------------
+fit <- cdc_state_counts %>% filter(state == "Massachusetts") %>%
+  excess_model(exclude = exclude_dates,
+               start = min(.$date),
+               end = max(.$date),
+               knots.per.year = 12,
+               weekday.effect = FALSE,
+               verbose = FALSE)
+
+## -----------------------------------------------------------------------------
+excess_plot(fit)
+
+## -----------------------------------------------------------------------------
+fit$detected_intervals
+
+## -----------------------------------------------------------------------------
+cumu <- excess_cumulative(fit, 
+                          start = make_date(2020, 3, 1),
+                          end = make_date(2020, 5, 2))
+cumu %>%
+  ggplot(aes(date)) +
+  geom_ribbon(aes(ymin = observed- 2*sd, ymax = observed + 2*sd), alpha = 0.5) +
+  geom_point(aes(y = observed)) 
+
+
+## -----------------------------------------------------------------------------
+intervals <- list(flu = seq(make_date(2017, 12, 16), make_date(2018, 2, 10), by = "day"),
+                  covid19 = seq(make_date(2020, 3, 14), max(cdc_state_counts$date), by = "day"))
+cdc_state_counts %>% filter(state == "Massachusetts") %>%
+  excess_model(exclude = exclude_dates,
+               interval = intervals,
+               weekday.effect = FALSE,
+               verbose = FALSE)
+

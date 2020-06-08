fb570d4bf832c09ec8dcb55251cd9d92b125f693
Rafael A. Irizarry <rairizarry@gmail.com>
2020-06-08 15:21:48 -0400

    Updating the rdas and some of the help files.

--- R/puerto_rico_counts.R
+++ R/puerto_rico_counts.R
@@ -1,7 +1,7 @@
 #' Puerto Rico daily mortality
 #' 
 #' A data frame with Puerto Rico daily mortality counts, stratified by agegroup,
-#' from 1985-01-01 to 2020-05-22
+#' from 1985 to 2020.
 #' which includes the days hurricanes Hugo, Georges, and Maria made
 #' landfall on 1989-09-18, 1998-09-21, and 2017-09-20, respectively.
 #' The outcome column includes the number of deaths for that day.
--- R/puerto_rico_icd.R
+++ R/puerto_rico_icd.R
@@ -1,7 +1,7 @@
 #' Puerto Rico daily mortality by cause of death
 #' 
 #' A data frame with Puerto Rico daily mortality counts, stratified by cause of death
-#' from 1999-01-01 to 2020-05-22
+#' from 1999 to 2020.
 #' which includes the day hurricanes Maria made
 #' landfall on 2017-09-20.
 #' The outcome column includes the number of deaths for that day for that ICD-10 code.
